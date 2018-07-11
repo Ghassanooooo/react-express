a1e7d8981bbca7bd2bf377180a1208a0293896b4
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-07-11 16:28:31 +0200

    finised the basic express

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express =require ('express')
+const express = require('express');
 
-const chalk =require('chalk')
-const app= new express()
-const path=require('path')
+const chalk = require('chalk')
 
+const ejs=require('ejs')
+const app = new express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use('/css',express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')))
-app.use('/js',express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')))
-app.use('/js',express.static(path.join(__dirname,'node_modules/jquery/dist')))
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views','./src/views')
+app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-   res.sendFile(path.join(__dirname , 'views' ,'index.html'))
-})
+app.get('/', (req, res) => {
+  res.render('index',{title:'MR:Error',frinds:['Ali','Abas','Issac','Ahmad']});
+});
 
-app.listen(3000,()=>{
-    console.log(`listening on port ${chalk.blue('3000')}`)
-})
+app.listen(3000, () => {
+  console.log(`listening on port ${chalk.blue('3000')}`);
+});
